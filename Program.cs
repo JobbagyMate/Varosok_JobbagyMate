5bd3ee78539ae894495a09e709341bdb5170ac7b
JobbagyMate <jobbagymatte@gmail.com>
2024-11-26 10:20:35 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using Varosok_JobbagyMate;
+
+List<VarosAdatok> varosAdatok = new List<VarosAdatok>();
+StreamReader sr = new StreamReader(@"../../../src/varosok.csv");
+sr.ReadLine();
+while (!sr.EndOfStream)
+{
+    varosAdatok.Add(new(sr.ReadLine()));
+}
+
+Console.WriteLine($"A kollekció hossza: {varosAdatok.Count}");
+
+//1) hány millió fő él összesen kínai nagyvárosokban?
+//2) mekkora az indiai nagyvárosok átlaglélekszáma?
+//3) melyik nagyváros a legnépesebb?
+//4) 20M lakos feletti nagyvárosok, népesség szerint csökkenő sorrendben.
+//5) hány olyan ország van, ami több nagyávárossal is szerepel a listában?
+//6) milyen betűvel kezdődik a legtöbb nagyváros neve?
+
+
+// 1. feladat:
+Console.WriteLine("1. feladat:");
+float osszFo = varosAdatok.Sum(c => c.Nepesseg);
+Console.WriteLine($"A kínai nagyvárosokban összesen {osszFo} millió fő él.");
+Console.WriteLine("");
+
+// 2. feladat:
+Console.WriteLine("2. feladat:");
+var india = varosAdatok.Where(c => c.OrszagNev == "India");
+float indiaOssznep = india.Sum(c => c.Nepesseg);
+float indiaiDb = india.Count();
+Console.WriteLine($"Az indiai nagyvárosok átlaglélekszáma: {indiaOssznep / indiaiDb} millió fő");
+Console.WriteLine("");
+
+// 3. feladat:
+Console.WriteLine("3. feladat:");
+var legnepesebbSzam = varosAdatok.Max(c => c.Nepesseg);
+var legnepesebb = varosAdatok.Select(c => c.Nepesseg == legnepesebbSzam);
+Console.WriteLine($"{legnepesebb}");
+Console.WriteLine("");
+
+// 4. feladat:
+Console.WriteLine("4. feladat:");
+var nepessegCsokkeno = varosAdatok.OrderByDescending(c => c.Nepesseg);
+foreach (var item in nepessegCsokkeno)
+{
+    Console.WriteLine($"\t- {item}");
+}
+Console.WriteLine("");
+
+// 5. feladat:
---
+++ VarosAdatok.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varosok_JobbagyMate
+{
+    internal class VarosAdatok
+    {
+        public string VarosNev { get; set; }
+        public string OrszagNev { get; set; }
+        public float Nepesseg { get; set; }
+
+        public VarosAdatok(string s)
+        {
+            string[] v = s.Split(';');
+            VarosNev = v[0];
+            OrszagNev = v[1];
+            Nepesseg = float.Parse(v[2]);
+        }
+    }
+}
